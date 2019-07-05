10f477d51c5a988c3cdfb9e4f9b1e1010cd5b8f8
umezizi <umeziro.v2@gmail.com>
2019-07-05 18:35:48 +0900

    add twitter OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   get '/about',   to: 'pages#about'
   get '/signup',  to: 'users#new'
   root to: 'gyms#index'
