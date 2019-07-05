aca587483514c5062fe307df929d572afbc8729b
umezizi <umeziro.v2@gmail.com>
2019-07-05 12:06:25 +0900

    users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'about', to: 'pages#about'
   root to: 'gyms#index'
+
+  # ヘルスチェック用のroutesを設定
+  resources :health_check, only: [:index]
 end
