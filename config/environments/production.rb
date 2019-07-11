9659b37158df295799d62732db5784210a9081fb
umezizi <umeziro.v2@gmail.com>
2019-07-11 14:19:10 +0900

    password reset mail host change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'grade-share.com' }
+  config.action_mailer.default_url_options = { host: 'www.grade-share.com' }
   config.action_mailer.delivery_method = :ses
 
   # Ignore bad email addresses and do not raise email delivery errors.
