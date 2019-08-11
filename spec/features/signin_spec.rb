7419fb9d60bb3e676412a9c5df8dc31ae0c99ea5
umezizi <umeziro.v2@gmail.com>
2019-08-11 20:29:54 +0900

    feature test

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -23,7 +23,6 @@ RSpec.feature "Signins", type: :feature do
     expect(page).to     have_link "設定",         href: edit_user_registration_path
     expect(page).to     have_link "ログアウト",   href: destroy_user_session_path
     click_link "ログアウト"
-    expect(current_path).to eq destroy_user_session_path
     expect(page).to_not have_link "COMMENTS",     href: user_path(user.id)
     expect(page).to_not have_link "ジム追加",     href: new_gym_path
     expect(page).to_not have_link "ユーザー一覧", href: users_path
